2732cd8e0a830cb36ece37bfd4436a032ce91e71
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-13 21:42:53 +0200

    Added pages and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
+import Gigs from './pages/gigs/Gigs';
+import Gig from './pages/gig/Gig';
+import Orders from './pages/orders/Orders';
+import MyGigs from './pages/myGigs/MyGigs';
+import Add from './pages/add/Add';
+import Messages from './pages/messages/Messages';
+import Message from './pages/message/Message';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import Pay from './pages/pay/Pay';
+import Success from './pages/success/Success';
 
 import './app.scss';
 
 function App() {
-    return (
-        <div className="app">
+    const Layout = () => {
+        return (
+            <div className="app">
+                    <Outlet />
+                    <Footer />
+            </div>
+        );
+    };
 
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Layout />,
+            children: [
+                { path: '/', element: <Home /> },
+                { path: '/gigs', element: <Gigs /> },
+                { path: '/mygigs', element: <MyGigs /> },
+                { path: '/orders', element: <Orders /> },
+                { path: '/messages', element: <Messages /> },
+                { path: '/message/:id', element: <Message /> },
+                { path: '/add', element: <Add /> },
+                { path: '/gig/:id', element: <Gig /> },
+                { path: '/register', element: <Register /> },
+                { path: '/login', element: <Login /> },
+                { path: '/pay/:id', element: <Pay /> },
+                { path: '/success', element: <Success /> }
+            ]
+        }
+    ]);
+
+    return (
+        <div>
+            <RouterProvider router={router} />
         </div>
     );
-};
+}
 
 export default App;
