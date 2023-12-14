a137846a5678605020e525d440567fb8c2c566eb
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-14 23:03:14 +0200

    Added Featured component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Featured from '../../components/featured/Featured';
 
 import './Home.scss';
 
 const Home = () => {
     return (
         <div className='home'>
+            <Featured />
             <div className="features">
                 <div className="container">
                     <div className="item">
