b3bfbea04848a5799bdd3cfd029ea425acb2d0f7
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-15 00:30:44 +0200

    Added Slide component

---
+++ client/src/components/slide/Slide.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Slide = ({ children, slidesToShow, arrowsScroll, autoplay }) => {
+    return (
+        <div className='slide'>
+            <div className="container">
+               
+            </div>
+        </div>
+    );
+};
+
+export default Slide;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
+import Slide from '../../components/slide/Slide';
 
 import './Home.scss';
 
@@ -9,6 +10,7 @@ const Home = () => {
         <div className='home'>
             <Featured />
             <TrustedBy />
+            <Slide/>
             <div className="features">
                 <div className="container">
                     <div className="item">
