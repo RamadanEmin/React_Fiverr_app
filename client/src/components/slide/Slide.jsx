64b1040c09d9b37bf0551df297c2a831034b63be
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-15 20:37:37 +0200

    Added Slider data

--- client/src/components/slide/Slide.jsx
+++ client/src/components/slide/Slide.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Slider from 'infinite-react-carousel';
+
+import './Slide.scss';
 
 const Slide = ({ children, slidesToShow, arrowsScroll, autoplay }) => {
     return (
         <div className='slide'>
             <div className="container">
-               
+                <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll} autoplay={autoplay}>
+                    {children}
+                </Slider>
             </div>
         </div>
     );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/slide/Slide';
+import { cards } from '../../data';
 
 import './Home.scss';
 
@@ -10,7 +11,11 @@ const Home = () => {
         <div className='home'>
             <Featured />
             <TrustedBy />
-            <Slide/>
+            <Slide slidesToShow={5} arrowsScroll={5} autoplay={true}>
+                {cards.map((card) => (
+                    <CatCard key={card.id} />
+                ))}
+            </Slide>
             <div className="features">
                 <div className="container">
                     <div className="item">
