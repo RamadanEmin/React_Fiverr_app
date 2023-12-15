19726c363aae244ef12e03611311d0e9f61e2bad
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-16 00:41:03 +0200

    Styling ProjectCard

--- client/src/components/projectCard/ProjectCard.jsx
+++ client/src/components/projectCard/ProjectCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './ProjectCard.scss';
+
 function ProjectCard({ project }) {
     return (
         <Link to="/" className='link'>
