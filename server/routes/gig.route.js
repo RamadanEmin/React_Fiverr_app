290054206461ddf4b154341de115cc41f39392b2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-25 21:16:47 +0200

    Added Get gigs controller

--- server/routes/gig.route.js
+++ server/routes/gig.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createGig, deleteGig, getGig } from '../controllers/gig.controller.js';
+import { createGig, deleteGig, getGig, getGigs } from '../controllers/gig.controller.js';
 import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
 
 router.get('/single/:id', getGig);
+router.get('/', getGigs);
 router.post('/', verifyToken, createGig);
 router.delete('/:id', verifyToken, deleteGig);
 
