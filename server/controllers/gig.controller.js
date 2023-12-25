e9bc6b7713e833deecd0defcd5ccd73ebd31a18b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-25 20:27:36 +0200

    Added Get gig controller

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -1,6 +1,20 @@
 import Gig from '../model/gig.model.js';
 import createError from "../utils/createError.js";
 
+export const getGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id);
+
+        if (!gig) {
+            return next(createError(404, 'Gig not found!'));
+        }
+
+        res.status(200).send(gig);
+    } catch (err) {
+        next(err);
+    }
+};
+
 export const createGig = async (req, res, next) => {
     if (!req.isSeller) {
         return next(createError(403, 'Only sellers can create a gig!'));
--- server/routes/gig.route.js
+++ server/routes/gig.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createGig } from '../controllers/gig.controller.js';
+import { createGig, getGig } from '../controllers/gig.controller.js';
 import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
 
+router.get('/single/:id', getGig);
 router.post('/', verifyToken, createGig);
 
 export default router;
