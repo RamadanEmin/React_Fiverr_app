c5baccef94bf21ae47aff0fbd52d590c28593e1f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-27 17:10:58 +0200

    Added GET review controller

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/jwt.js';
-import { createReview } from '../controllers/review.controller.js';
+import { createReview, getReviews } from '../controllers/review.controller.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, createReview);
+router.get('/:gigId', getReviews);
 
 export default router;
