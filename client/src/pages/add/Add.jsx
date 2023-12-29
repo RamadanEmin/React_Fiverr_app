9521339f048cadca220a788174f18493d90fcd38
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-29 22:55:49 +0200

    Added Handle change input function to Add page

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { INITIAL_STATE, gigReducer } from '../../reducers/gigReducer';
 
 const Add = () => {
+    const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE);
+
+    const handleChange = (e) => {
+        dispatch({
+            type: 'CHANGE_INPUT',
+            payload: { name: e.target.name, value: e.target.value }
+        });
+    };
 
     return (
         <div className="add">
@@ -13,9 +22,10 @@ const Add = () => {
                             type="text"
                             name="title"
                             placeholder="e.g. I will do something I'm really good at"
+                            onChange={handleChange}
                         />
                         <label htmlFor="">Category</label>
-                        <select name="cat" id="cat">
+                        <select name="cat" id="cat" onChange={handleChange}>
                             <option value="design">Design</option>
                             <option value="web">Web Development</option>
                             <option value="animation">Animation</option>
@@ -24,7 +34,7 @@ const Add = () => {
                         <div className="images">
                             <div className="imagesInputs">
                                 <label htmlFor="">Cover Image</label>
-                                <input type="file" />
+                                <input type="file"/>
                                 <label htmlFor="">Upload Images</label>
                                 <input type="file" multiple/>
                             </div>
@@ -35,6 +45,7 @@ const Add = () => {
                             name="desc"
                             id=""
                             placeholder="Brief descriptions to introduce your service to customers"
+                            onChange={handleChange}
                             cols="0"
                             rows="16"
                         ></textarea>
@@ -42,7 +53,7 @@ const Add = () => {
                     </div>
                     <div className="details">
                         <label htmlFor="">Service Title</label>
-                        <input type="text" name="shortTitle" placeholder="e.g. One-page web design" />
+                        <input type="text" name="shortTitle" placeholder="e.g. One-page web design" onChange={handleChange} />
                         <label htmlFor="">Short Description</label>
                         <textarea
                             name="shortDesc"
@@ -50,18 +61,28 @@ const Add = () => {
                             placeholder="Short description of your service"
                             cols="30"
                             rows="10"
+                            onChange={handleChange}
                         ></textarea>
                         <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-                        <input type="number" name="deliveryTime" min={1} />
+                        <input type="number" name="deliveryTime" min={1} onChange={handleChange} />
                         <label htmlFor="">Revision Number</label>
-                        <input type="number" name="revisionNumber" min={1} />
+                        <input type="number" name="revisionNumber" min={1} onChange={handleChange} />
                         <label htmlFor="">Add Features</label>
-                        <form action="" className="addBtn" >
+                        <form action="" className="addBtn">
                             <input type="text" placeholder="e.g. page design" />
                             <button type="submit">add</button>
                         </form>
+                        <div className="addedFeatures">
+                            {state?.features?.map(f => (
+                                <div className="item" key={f}>
+                                    <button>
+                                        {f}
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
                         <label htmlFor="">Price</label>
-                        <input type="number" name="price" />
+                        <input type="number" name="price" onChange={handleChange} />
                     </div>
                 </div>
             </div>
---
+++ client/src/reducers/gigReducer.js
@@ -0,0 +1,27 @@
+export const INITIAL_STATE = {
+    userId: JSON.parse(localStorage.getItem('currentUser'))?._id,
+    title: '',
+    cat: '',
+    cover: '',
+    images: [],
+    desc: '',
+    shortTitle: '',
+    shortDesc: '',
+    deliveryTime: 0,
+    revisionNumber: 0,
+    features: [],
+    price: 0
+};
+
+export const gigReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value
+            };
+
+        default:
+            return state;
+    }
+};
