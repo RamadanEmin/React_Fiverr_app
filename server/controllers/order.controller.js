07f2ba04e63c68dea63e396ada006c552e05cb2c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-30 22:57:15 +0200

    Added create payment orders controller

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,4 +1,34 @@
 import Order from '../model/order.model.js';
+import Gig from '../model/gig.model.js';
+import Stripe from 'stripe';
+
+export const intent = async (req, res, next) => {
+    const stripe = new Stripe(process.env.STRIPE);
+
+    const gig = await Gig.findById(req.params.id);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: gig.price * 100,
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    const newOrder = new Order({
+        gigId: gig._id,
+        img: gig.cover,
+        title: gig.title,
+        buyerId: req.userId,
+        sellerId: gig.userId,
+        price: gig.price,
+        payment_intent: paymentIntent.id
+    });
+
+    await newOrder.save();
+
+    res.status(200).send({ clientSecret: paymentIntent.client_secret });
+};
 
 export const getOrders = async (req, res, next) => {
     try {
--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/jwt.js';
-import { getOrders } from '../controllers/order.controller.js';
+import { getOrders, intent } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getOrders);
+router.post('/create-payment-intent/:id', verifyToken, intent);
 
 export default router;
