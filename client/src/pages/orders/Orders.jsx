5238d8b3431652b76d1a9b810cb3423575f2f637
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-30 21:50:04 +0200

    Update Orders page

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import newRequest from '../../utils/newRequest.js';
 
 const Orders = () => {
+
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['orders'],
+        queryFn: () =>
+            newRequest.get(`/orders`).then((res) => {
+                return res.data;
+            })
+    });
+
     return (
-        <div>Orders</div>
+        <div className="orders">
+            {isLoading
+                ? 'Loading...'
+                : error
+                    ? 'Something went wrong!'
+                    : (
+                        <div className="container">
+                            <div className="title">
+                                <h1>Orders</h1>
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Title</th>
+                                        <th>Price</th>
+                                        <th>Contact</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map(order => (
+                                        <tr key={order._id}>
+                                            <td>
+                                                <img
+                                                    className="image"
+                                                    src={order.img}
+                                                    alt=""
+                                                />
+                                            </td>
+                                            <td>{order.title}</td>
+                                            <td>{order.price}</td>
+                                            <td>
+                                                <img className="message" src="./img/message.png" alt="" />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )
+            }
+        </div>
     );
 };
 
