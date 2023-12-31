5c9fb662868d6b5bc35d4a9e3b41c9ea5d87bd82
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-31 10:45:37 +0200

    Added Pay page

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { useParams } from 'react-router-dom';
+import newRequest from '../../utils/newRequest';
+
+const stripePromise = loadStripe("pk_test_51MwjYiIODTgGmCRSQm2F21EyGtUbtuVe08uxLrFvzUK8a5vNGMXsY6a5fstYriQcABdfvCU3ALl7rGK096i9XYoA00DlSk75KW");
 
 const Pay = () => {
+    const [clientSecret, setClientSecret] = useState('');
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                const res = await newRequest.post(`/orders/create-payment-intent/${id}`);
+
+                setClientSecret(res.data.clientSecret);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        makeRequest();
+    }, []);
+
+    const appearance = {
+        theme: 'stripe',
+    };
+
+    const options = {
+        clientSecret,
+        appearance,
+    };
+
     return (
-        <div>Pay</div>
+        <div className='pay'>
+            {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
+                </Elements>
+            )}
+        </div>
     );
-};
+}
 
 export default Pay;
