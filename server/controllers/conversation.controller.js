e164db46014c6ae509321f815d70ec24440de374
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-01 22:48:07 +0200

    Added GET single conversation controller

--- server/controllers/conversation.controller.js
+++ server/controllers/conversation.controller.js
@@ -1,4 +1,5 @@
 import Conversation from '../model/conversation.model.js';
+import createError from '../utils/createError.js';
 
 export const getConversations = async (req, res, next) => {
     try {
@@ -7,6 +8,20 @@ export const getConversations = async (req, res, next) => {
             .sort({ updatedAt: -1 });
 
         res.status(200).send(conversations);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getSingleConversation = async (req, res, next) => {
+    try {
+        const conversation = await Conversation.findOne({ id: req.params.id });
+
+        if (!conversation) {
+            return next(createError(404, 'Not found!'));
+        }
+
+        res.status(200).send(conversation);
     } catch (err) {
         next(err);
     }
--- server/routes/conversation.route.js
+++ server/routes/conversation.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/jwt.js';
-import { getConversations } from '../controllers/conversation.controller.js';
+import { getConversations, getSingleConversation } from '../controllers/conversation.controller.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getConversations);
+router.get('/single/:id', verifyToken, getSingleConversation);
 
 export default router;
