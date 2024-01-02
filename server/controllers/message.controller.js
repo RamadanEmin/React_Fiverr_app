20c2f2678830e8c6861ea9dc5f41d83bb266bdcb
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-02 20:42:13 +0200

    Added POST message controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,4 +1,30 @@
 import Message from '../model/message.model.js';
+import Conversation from '../model/conversation.model.js';
+
+export const createMessage = async (req, res, next) => {
+    const newMessage = new Message({
+        conversationId: req.body.conversationId,
+        userId: req.userId,
+        desc: req.body.desc
+    });
+
+    try {
+        const savedMessage = await newMessage.save();
+        await Conversation.findOneAndUpdate({ id: req.body.conversationId }, {
+            $set: {
+                readBySeller: req.isSeller,
+                readByBuyer: !req.isSeller,
+                lastMessage: req.body.desc,
+            }
+        },
+            { new: true }
+        );
+
+        res.status(201).send(savedMessage);
+    } catch (err) {
+        next(err);
+    }
+};
 
 export const getMessages = async (req, res, next) => {
     try {
